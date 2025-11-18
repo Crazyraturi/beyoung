9c3c5358df061ff98c52f59ab7727045b8c5d406
Subodh Raturi <subodhblogger966@gmail.com>
2025-11-18 17:50:32 +0530

    conflict reolved

--- src/App.jsx
+++ src/App.jsx
@@ -23,37 +23,21 @@ const App = () => {
       <ScrollToTop />
       <NavBar />
       <Routes>
-        {/* Main Pages */}
         <Route path="/" element={<Home />} />
         <Route path="/product-details" element={<ProductDetails />} />
-
-        {/* Order Related */}
         <Route path="/track-order" element={<TrackOrder />} />
         <Route path="/returns-exchange" element={<ReturnOrder />} />
-
-        {/* Policies */}
         <Route path="/terms" element={<TermsandCondition />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/shipping" element={<ShippingPolicy />} />
-
-        {/* Support Pages */}
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/faqs" element={<FAQpage />} />
-
-        {/* Extra Pages */}
         <Route path="/about" element={<Aboutus />} />
-<<<<<<< HEAD
         <Route path="/collab" element={<Collaborations />} />
-        <Route path="/product-details" element={<ProductDetails />} />
-=======
-        <Route path="/collaboration" element={<Collaborations />} />
->>>>>>> e0176a13ec4a0aa56e8882c2c2f1c645e73ffbb7
         <Route path="/clothing-stores-near-me" element={<More />} />
         <Route path="/in-the-news" element={<Media />} />
-         <Route path="/career" element={<CareersPage/>} />
-
+        <Route path="/career" element={<CareersPage />} />
       </Routes>
-
       <Footer />
     </BrowserRouter>
   );
